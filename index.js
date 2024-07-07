ddecea1de3af293289b3733d01f4726a89eebc15
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-07 10:00:24 +0300

    forst commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(driverList) {
+  const firstTwo = driverList.slice(0, 2);
+  return firstTwo;
+}
+
+function returnLastTwoDrivers(driverList) {
+  const lastTwo = driverList.slice(-2);
+  return lastTwo;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, selectionFunction){
+  return selectionFunction(arrayOfDrivers.slice()); 
+}
+module.exports = {
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers,
+  selectingDrivers,
+  createFareMultiplier,
+  fareDoubler, 
+  fareTripler, 
+  selectDifferentDrivers,
+};
